e3754f44fdc90ba4de82b24902817e90d958e5c6
Papp Szabolcs <papp.szabolcs.bazil@gmail.com>
2023-02-18 19:48:29 +0100

    [#0002] fix typos and misnomers

--- src/main/java/hu/aestallon/adrestore/repository/AddressRepository.java
+++ src/main/java/hu/aestallon/adrestore/repository/AddressRepository.java
@@ -3,5 +3,4 @@ package hu.aestallon.adrestore.repository;
 import hu.aestallon.adrestore.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-}
+public interface AddressRepository extends JpaRepository<Address, Integer> {}
--- src/main/java/hu/aestallon/adrestore/repository/PersonRepository.java
+++ src/main/java/hu/aestallon/adrestore/repository/PersonRepository.java
@@ -3,5 +3,4 @@ package hu.aestallon.adrestore.repository;
 import hu.aestallon.adrestore.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
+public interface PersonRepository extends JpaRepository<Person, Integer> {}
